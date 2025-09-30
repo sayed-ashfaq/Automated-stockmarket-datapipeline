f50cf0e8d75acd326c91a4fd72fdfcd109d7e3e2
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-09-30 21:12:47 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Project configuration settings
+BIGQUERY_PROJECT = "your_project_name"
+TABLE_NAMES = {
+    "raw": "raw_table_name",
+    "processed": "processed_table_name"
+}
+API_KEYS = {
+    "service": "your_api_key"
+}
