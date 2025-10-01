ac416687b95fa6adae270a46606559cfd6208d75
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-10-01 10:54:08 +0530

    Update data_ingestion.py

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,3 +1,12 @@
-# Fetch raw data from API or BigQuery
-def fetch_data():
-    pass
+# ingest the data from yfinance to google cloud bucket
+import datetime, time
+import pandas as pd
+from google.cloud import storage
+import yfinance as yf
+from logger import GLOBAL_LOGGER as log
+
+#step 1: load the data into dataframe
+
+def load_data(ticker_symbol, start_date, end_date):
+    df= yf.download(ticker_symbol, start=start_date, end=end_date)
+    return df
