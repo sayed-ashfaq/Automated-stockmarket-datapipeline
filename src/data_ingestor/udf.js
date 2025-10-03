7722f093ffc09606c8a4ac837fe614b94aa0f047
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-10-03 15:27:38 +0530

    Update udf.js

--- src/data_ingestor/udf.js
+++ src/data_ingestor/udf.js
@@ -1,13 +1,12 @@
 function transform(line){
     var values = line.split(',');
     var obj = new Object();
-    obj.Index = values[0];
-    obj.Date = values[1];
-    obj.Close = values[2];
-    obj.High = values[3];
-    obj.Low = values[4];
-    obj.Open = values[5];
-    obj.Volume = values[6];
+    obj.Date = values[0];
+    obj.Close = values[1];
+    obj.High = values[2];
+    obj.Low = values[3];
+    obj.Open = values[4];
+    obj.Volume = values[5];
     var jsonString = JSON.stringify(obj);
     return jsonString;
 }
