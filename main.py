2430f106a8c41f449411ee87807010adddd01b45
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-10-03 12:09:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from src.data_ingestion import StockDataIngestionPipeline
+from src.data_ingestor.data_ingestion import StockDataIngestionPipeline
 
 def main():
     print("Hello from automated-stockmarket-datapipeline!")
     pipeline = StockDataIngestionPipeline()
     
     # Run for all configured tickers
-    ticker = ['TSLA']
+    ticker = ['TATAMOTORS.NS']
     pipeline.run(ticker_list = ticker)
 
 
